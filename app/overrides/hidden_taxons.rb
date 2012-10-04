1aaab5299bb8b07b54ac50db653325308cc7660d
Mark Linn <marklinn@xwcsolutions.com>
2012-10-04 10:23:45 -0700

    Successfully hiding items in taxon_tree and root menu.

--- app/overrides/hidden_taxons.rb
+++ app/overrides/hidden_taxons.rb
@@ -2,3 +2,8 @@ Deface::Override.new(:virtual_path  => "spree/admin/taxons/_form",
                      :insert_bottom => "[data-hook='admin_taxon_form_fields']",
                      :partial       => "spree/admin/taxons/hidden_form",
                      :name          => "hidden_taxons_admin_form")
+
+Deface::Override.new(:virtual_path  => "spree/shared/_taxonomies",
+                     :replace       => "#taxonomies[data-hook]",
+                     :partial       => "spree/shared/hidden_taxonomies",
+                     :name          => "hidden_taxonomies")
